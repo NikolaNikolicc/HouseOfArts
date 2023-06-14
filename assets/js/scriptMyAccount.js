07aa74187ad6313687274747b01dfaf3f708cf83
Nikola Nikolić <nn200357d@student.etf.bg.ac.rs>
2023-06-14 18:22:08 +0200

    uradjeno dinamicko ucitavanje ponuda kao i validacija ponuda, nedostaje implementirati prikaz cene u artwork.html i azuriranje artworkData na novu value

---
+++ assets/js/scriptMyAccount.js
@@ -0,0 +1,73 @@
+$(document).ready(function () {
+
+    // let myOffers = [
+    //     {
+    //         artwork: JSON.stringify(data),
+    //         bid: bid,
+    //         message: messageForAuthor,
+    //     }
+    // ];
+
+    initFunc();
+
+    function initFunc() {
+        // vidi da li ima nesto u LS-u
+        let myOffers = getOffers();
+        if (myOffers == null) return;
+        let cntForActive = 1;
+        for (let i = 0; i < myOffers.length; i++) {
+            let artwork = JSON.parse(myOffers[i].artwork);
+            let bid = myOffers[i].bid;
+            let message = myOffers[i].message;
+            appendElements(artwork, bid, message, cntForActive);
+            cntForActive++;
+        }
+    }
+
+    function appendElements(artwork, bid, message,cntForActive) {
+        // dodavanje u prvi deo
+        var liElement = $('<li>').addClass('nav-item');
+        var aElement = $('<a>').addClass('nav-link').attr('data-bs-toggle', 'tab').attr('href', '#tab-'+cntForActive).text(artwork.name);
+        if(cntForActive == 1)aElement.addClass('active show');
+        aElement.append(liElement);
+        $("#list-of-artworks").append(aElement);
+        // dodavanje u drugi deo
+        var divElement = $('<div>').addClass('tab-pane').attr('id', 'tab-'+cntForActive);
+        if(cntForActive == 1)divElement.addClass('active show');
+        var rowElement = $('<div>').addClass('row');
+        var col1Element = $('<div>').addClass('col-lg-8 details order-2 order-lg-1');
+        var col2Element = $('<div>').addClass('col-lg-4 text-center order-1 order-lg-2');
+        var h3Element1 = $('<h3>').text('Ваша понуда: ' + bid);
+        var pElement1 = $('<p>').addClass('fst-italic').text('Проверите статус понуде за ову уметнину кликом на ');
+        var aElement = $('<a>').attr('href', 'artworkGET.html?id='+artwork.id).text('линк');
+        pElement1.append(aElement);
+        var h3Element2 = $('<h3>').text('Ваша порука уметнику');
+        if(message == "")message = "Нисте оставили поруку приликом даваања понуде.";
+        var pElement2 = $('<p>').text(message);
+        var imgElement = $('<img>').attr('src', 'assets/img/'+artwork.image).attr('alt', artwork.name).addClass('img-fluid');
+
+        col1Element.append(h3Element1, pElement1, h3Element2, pElement2);
+        col2Element.append(imgElement);
+        rowElement.append(col1Element, col2Element);
+        divElement.append(rowElement);
+
+        // Add the div element to the desired container
+        $('#list-of-offers').append(divElement);
+    }
+
+    /**
+     * dohvata sve ponude iz LS-a
+     * @returns Array
+     */
+    function getOffers() {
+        let myOffers = localStorage.getItem("offers");
+        if (myOffers != null) {
+            myOffers = JSON.parse(myOffers);
+            return myOffers;
+        } else {
+            localStorage.setItem("offers", JSON.stringify(myOffers));
+            return [];
+        }
+    }
+
+});
